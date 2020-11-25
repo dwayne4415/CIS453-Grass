aa3f0d92a077475baaa532eccaa832c240eb5c9f
Courtney Issac <courtneyissac@Courtneys-MacBook-Air.local>
2020-11-25 13:42:08 -0600

    Images

---
+++ src/Grass.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+ 
+class Grass extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Grass</h2>
+        <p>OPEN TO THE PUBLIC!
+For grass pricing, delivery 
+and availability please contact 512-259-5188.:</p>
+        <ol>
+          <li>Bermuda</li>
+          <li>Zoysia</li>
+          <li>St. Augustine</li>
+          <li>Buffalo</li>
+          
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Grass;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Free Estimates</h2>
+        <p>We offer premium grass products THE GRASS PATCH - THE BEST LANDSCAPING and IRRIGATION COMPANY IN CENTRAL TEXAS
+
+We are a family owned business that has been serving the Austin Area and Central 
+Texas area since 1978. We take great pride in offering our customers the latest technology and freshest products in the industry. </p>
+ 
+        <p>Follow us on Social Media.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
