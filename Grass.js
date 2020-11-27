6587982e369920ede11f7a206c85b027eaa197d1
dwayne4415 <74990068+dwayne4415@users.noreply.github.com>
2020-11-27 10:44:48 -0600

    Add files via upload

---
+++ Grass.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Grass extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Grass</h2>
+        <p>OPEN TO THE PUBLIC!
+For grass pricing, delivery 
+and availability please contact 512-259-5188.:</p>
+        <ol>
+          <li>Bermuda</li>
+          <li>Zoysia</li>
+          <li>St. Augustine</li>
+          <li>Buffalo</li>
+          <img src="https://source.unsplash.com/random/400x200"alt="" />
+         </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Grass;
